ea2c722b0afc31b2e3e921c42cbacac0529b9234
limingyang <865713897@qq.com>
2024-04-23 18:15:33 +0800

    feat(): 优化代码结构和逻辑

--- cf_final.lua
+++ cf_final.lua
@@ -1,9 +1,11 @@
 ---@diagnostic disable: duplicate-set-field, undefined-global, param-type-mismatch
--------------------- 配置 --------------------
+-- [[  用户配置  ]]
 Config = {
   openMacroKey = 'capslock',                 -- scrolllock | capslock | numlock
   shootKey = 1,                              -- 攻击按键1:鼠标左键，也可设置键盘按键
   gameModeList = { 'zombie', 'pve', 'pvp' }, -- 模式列表
+  defaultGameModeIndex = 2,                  -- 默认游戏模式下标
+  openDebugger = true,                       -- 是否开启调试模式（输出打印信息）
   -- 生化模式绑定按键函数信息
   zombie = {
     ['4'] = { 'gatlingStab', 'xkQuickAttack' },
@@ -18,11 +20,27 @@ Config = {
   },
   -- 竞技模式
   pvp = {
-    ['4'] = { 'instantSpy' }
+    ['1'] = { 'instantSpy' },
+    ['5'] = { 'nonStopSquat' }
+  },
+  defaultCardIndex = 1, -- 默认卡片下标
+  -- 默认按键事件下标
+  defaultEventIndex = {
+    ['1'] = 1,
+    ['4'] = 1,
+    ['5'] = 1,
+    ['6'] = 1,
+    ['7'] = 1
+  },
+  -- 试炼岛卡片坐标
+  cardPosition = {
+    { { 0, 0, 3, 6 }, { -120, -135, 0, 0 }, { 180, 195, 70, 76 } },
+    { { 0, 0, 3, 6 }, { -40, -55, 0, 0 },   { 130, 145, 70, 76 } },
+    { { 0, 0, 3, 6 }, { 30, 40, 0, 0 },     { 90, 100, 70, 76 } },
+    { { 0, 0, 3, 6 }, { 76, 90, 0, 0 },     { 40, 55, 70, 76 } },
   },
   -- 鼠标按键绑定
-  gBind = {
-    ['rctrl+G1'] = 'changeGameMode',
+  modifierMap = {
     ['G1'] = 'play_1',
     ['lalt+G1'] = 'next_1',
     ['ralt+G1'] = 'reset_1',
@@ -39,14 +57,9 @@ Config = {
     ['lalt+G6'] = 'next_6',
     ['ralt+G6'] = 'reset_6',
     ['G7'] = 'play_7',
+    ['G10'] = 'nextGameMode',
+    ['G11'] = 'resetGameMode'
   },
-  -- 试炼岛卡片坐标
-  cardPosition = {
-    { { 0, 0, 3, 6 }, { -120, -135, 0, 0 }, { 180, 195, 70, 76 } },
-    { { 0, 0, 3, 6 }, { -40, -55, 0, 0 },   { 130, 145, 70, 76 } },
-    { { 0, 0, 3, 6 }, { 30, 40, 0, 0 },     { 90, 100, 70, 76 } },
-    { { 0, 0, 3, 6 }, { 76, 90, 0, 0 },     { 40, 55, 70, 76 } },
-  }
 }
 
 -- 可用修饰符列表
@@ -57,87 +70,77 @@ ChineseTextMap = {
   ['zombie'] = '生化模式',
   ['pve'] = '挑战模式',
   ['pvp'] = '竞技模式',
-  ['gatlingQuickShoot'] = '加特林速点宏',
-  ['quickCtrl'] = '闪蹲宏',
-  ['gatlingStab'] = '加特林连刺宏',
-  ['xkQuickAttack'] = '虚空重刀宏',
-  ['instantSpy'] = '一键瞬狙宏',
+  ['gatlingQuickShoot'] = '加特林速点',
+  ['gatlingStab'] = '加特林连刺',
+  ['xkQuickAttack'] = '虚空重刀',
+  ['instantSpy'] = '一键瞬狙',
   ['continueAttack'] = '挑战攻击释放双手',
   ['dropCard'] = '挑战试炼岛放置卡片',
   ['increaseCardIndex'] = '更新试炼岛卡片索引位置',
-  ['resetCardIndex'] = '重置试炼岛卡片索引位置'
+  ['resetCardIndex'] = '重置试炼岛卡片索引位置',
+  ['nonStopSquat'] = '闪蹲'
 }
 
---------------------   end   --------------------
 
--------------------- 事件函数(运行时) --------------------
+-- [[  运行时函数及部分参数模块  ]]
 Runtiming = {
-  curModeIndex = 2, -- 当前游戏模式下标
-  curCardIndex = 1, -- 试炼岛卡片当前下标
-  eventIndex = {
-    ['1'] = 1,
-    ['4'] = 1,
-    ['5'] = 1,
-    ['6'] = 1,
-    ['7'] = 1
-  },                 -- 按键事件下标
   eventFuncList = {} -- 运行时事件函数列表
 }
 
 -- 加特林速点宏
 function Runtiming.gatlingQuickShoot(key)
   repeat
-    BtDown(Config.shootKey)
-    Sleep(Random(80, 110))
-    BtUp(Config.shootKey)
-    Sleep(Random(20, 44))
-  until not IsPressed(key)
+    Utils.handleKeyDown(Config.shootKey)
+    Sleep(Utils.random(80, 110))
+    Utils.handleKeyUp(Config.shootKey)
+    Sleep(Utils.random(20, 44))
+  until not Utils.isKeyPressed(key)
 end
 
 -- 加特林连刺宏
 function Runtiming.gatlingStab(key)
   repeat
-    BtClick(3)
-    Sleep(Random(270, 300))
-    BtClick(Config.shootKey)
-    Sleep(Random(40, 63))
-  until not IsPressed(key)
+    Utils.handleKeyClick(3)
+    Sleep(Utils.random(270, 300))
+    Utils.handleKeyClick(Config.shootKey)
+    Sleep(Utils.random(40, 63))
+  until not Utils.isKeyPressed(key)
 end
 
 -- 虚空重刀宏
 function Runtiming.xkQuickAttack(key)
-  if (not IsPressed(key)) then
+  if (not Utils.isKeyPressed(key)) then
     -- 防止多次点击多次触发重复操作
     return false
   end
-  BtClick(3)
-  Sleep(Random(580, 600))
-  BtClick('f')
-  Sleep(Random(50, 70))
-  BtClick(3)
-  Sleep(Random(120, 140))
+  Utils.handleKeyClick(3)
+  Sleep(Utils.random(580, 600))
+  Utils.handleKeyClick('f')
+  Sleep(Utils.random(50, 70))
+  Utils.handleKeyClick(3)
+  Sleep(Utils.random(120, 140))
 end
 
 -- 挑战-试炼岛一键放置卡片
 function Runtiming.dropCard(key)
-  if (not IsPressed(key)) then
+  if (not Utils.isKeyPressed(key)) then
     -- 防止多次点击多次触发重复操作
     return false
   end
   local index = Runtiming.curCardIndex or 1
   local curPosition = Config.cardPosition[index]
-  local randomFn = GenerateRandomNumber()
-  BtClick('e')
-  Sleep(Random(30, 50))
+  local randomFn = Utils.generateRandomNumber()
+  Utils.handleKeyClick('e')
+  Sleep(Utils.random(30, 50))
   for i = 1, #curPosition do
     local position = curPosition[i]
     local x = randomFn(position[1], position[2])
     local y = randomFn(position[3], position[4])
     MoveMouseRelative(x, y)
-    Sleep(Random(30, 50))
-    if i % 2 == 0 then
-      BtClick(1)
-      Sleep(Random(30, 50))
+    Sleep(Utils.random(30, 50))
+    if i ~= 1 then
+      Utils.handleKeyClick(1)
+      Sleep(Utils.random(30, 50))
     end
   end
 end
@@ -151,75 +154,78 @@ function Runtiming.increaseCardIndex()
   local nextIndex = (Runtiming.curCardIndex + 1) % (cardNums + 1)
   local realIndex = nextIndex == 0 and 1 or nextIndex
   Runtiming.curCardIndex = realIndex
-  PrintInfo()
+  Main.printInfo()
 end
 
 -- 挑战-试炼岛卡片下标重置
 function Runtiming.resetCardIndex()
   Runtiming.curCardIndex = 1
-  PrintInfo()
+  Main.printInfo()
 end
 
 -- 挑战-一键攻击
 function Runtiming.continueAttack(key)
-  if (not IsPressed(key)) then
+  if (not Utils.isKeyPressed(key)) then
     -- 防止多次点击多次触发重复操作
     return false
   end
   local hasPressed = Runtiming.hasPressed or false
   if hasPressed then
-    BtUp(Config.shootKey)
-    Sleep(Random(180, 200))
-    BtClick('r')
+    Utils.handleKeyUp(Config.shootKey)
+    Sleep(Utils.random(180, 200))
+    Utils.handleKeyClick('r')
     Runtiming.hasPressed = false
   else
-    BtDown(Config.shootKey)
+    Utils.handleKeyDown(Config.shootKey)
     Runtiming.hasPressed = true
   end
 end
 
 -- 一键瞬狙宏
 function Runtiming.instantSpy(key)
-  if (not IsPressed(key)) then
+  if (not Utils.isKeyPressed(key)) then
     -- 防止多次点击多次触发重复操作
     return false
   end
-  BtClick(3)
-  Sleep(Random(20, 40))
-  BtClick(1)
-  Sleep(Random(20, 40))
-  BtClick('q')
-  Sleep(Random(20, 40))
-  BtClick('q')
-  Sleep(Random(20, 40))
+  Utils.handleKeyClick(3)
+  Sleep(Utils.random(20, 40))
+  Utils.handleKeyClick(1)
+  Sleep(Utils.random(20, 40))
+  Utils.handleKeyClick('q')
+  Sleep(Utils.random(20, 40))
+  Utils.handleKeyClick('q')
+  Sleep(Utils.random(20, 40))
 end
 
---------------------   end   --------------------
+-- 连续蹲下
+function Runtiming.nonStopSquat()
+  repeat
+    Utils.handleKeyClick('lctrl')
+    Sleep(Utils.random(26, 46))
+  until not CF.isPressed(key)
+end
 
--------------------- 公共函数 --------------------
+-- [[  工具函数模块  ]]
+Utils = {}
 -- 四舍五入
-function math.round(num)
+function Utils.round(num)
   local floor = math.floor(num)
   local ceil = math.ceil(num)
-  if num - floor < 0.5 then
-    return floor
-  else
-    return ceil
-  end
+  return num - floor < 0.5 and floor or ceil
 end
 
 -- 分割字符串，返回数组
-function string.split(str, separator)
-  local result = {}
+function Utils.split(str, separator)
   local pattern = string.format('([^%s]+)', separator)
+  local result = {}
   for match in string.gmatch(str, pattern) do
     table.insert(result, match)
   end
-  return result
+  return table.unpack(result)
 end
 
 -- 获取表长度
-function table.getLength(t)
+function Utils.getTableLength(t)
   local i = 0
   for _, _ in pairs(t) do
     i = i + 1
@@ -228,29 +234,29 @@ function table.getLength(t)
 end
 
 -- 正态分布变换
-function BoxMuller(mean, stddev)
+function Utils.boxMuller(mean, stddev)
   -- 生成两个均匀分布的随机数
   local u1 = math.random()
   local u2 = math.random()
   -- 使用Box-Muller变换转换成正态分布随机数
   local z0 = math.sqrt(-2.0 * math.log(u1)) * math.cos(2.0 * math.pi * u2)
   -- 使用均值和标准差进行线性变换
-  return math.round(mean + z0 * stddev)
+  return Utils.round(mean + z0 * stddev)
 end
 
 -- 随机数方法
-function Random(m, n)
+function Utils.random(m, n)
   local mean = (m + n) / 2
   local sigma = (n - m) / (2 * 1.645) -- 1.645 0.9500 1.2815 0.9000
-  local random = BoxMuller(mean, sigma)
+  local random = Utils.boxMuller(mean, sigma)
   while (random < m) do
-    random = math.round(math.random(m, n))
+    random = Utils.round(math.random(m, n))
   end
   return random
 end
 
 -- 返回不重复随机数方法
-function GenerateRandomNumber()
+function Utils.generateRandomNumber()
   local prevNum = 0
   local currentNum = 0
   return function(min, max)
@@ -258,35 +264,28 @@ function GenerateRandomNumber()
       return 0
     end
     while (prevNum == currentNum) do
-      currentNum = math.round(min + (max - min) * math.random())
+      currentNum = Utils.round(min + (max - min) * math.random())
     end
     prevNum = currentNum
     return currentNum
   end
 end
 
--- 遍历数组-forEach
-function table.forEach(t, c)
-  for i = 1, #t do
-    c(t[i], i)
-  end
-end
-
 -- 触发点击
-function BtClick(key)
+function Utils.handleKeyClick(key)
   if type(key) == 'number' then
     PressMouseButton(key)
-    Sleep(Random(30, 50))
+    Sleep(Utils.random(30, 50))
     ReleaseMouseButton(key)
   else
     PressKey(key)
-    Sleep(Random(30, 50))
+    Sleep(Utils.random(30, 50))
     ReleaseKey(key)
   end
 end
 
 -- 按键按下
-function BtDown(key)
+function Utils.handleKeyDown(key)
   if type(key) == 'number' then
     PressMouseButton(key)
   else
@@ -295,7 +294,7 @@ function BtDown(key)
 end
 
 -- 按键抬起
-function BtUp(key)
+function Utils.handleKeyUp(key)
   if type(key) == 'number' then
     ReleaseMouseButton(key)
   else
@@ -304,7 +303,7 @@ function BtUp(key)
 end
 
 -- 判断按键是否按下(系统级监听)
-function IsPressed(key)
+function Utils.isKeyPressed(key)
   if (type(key) == 'number' and key <= 5 and key >= 1) then
     return IsMouseButtonPressed(key)
   elseif type(key) == 'string' then
@@ -315,12 +314,12 @@ function IsPressed(key)
 end
 
 -- 是否开启宏
-function IsOpenMacro(key)
+function Utils.isOpenMacro(key)
   return IsKeyLockOn(key or Config.openMacroKey or 'capslock')
 end
 
 -- 按key的大小顺序返回新表
-function GetTableSort(t)
+function Utils.getTableSort(t)
   local keyT = {}
   local newT = {}
   for k, _ in pairs(t) do
@@ -335,16 +334,15 @@ function GetTableSort(t)
   return newT
 end
 
---------------------   end   --------------------
+-- 返回固定长度的字符串
+function Utils.completeStr(str, num)
+  return string.format('%-' .. num .. 's', str)
+end
 
---------------------   plugin   --------------------
-Plugins = {}
-
---------------------   end   --------------------
-
---------------------   base   --------------------
+-- [[  主函数  ]]
+Main = {}
 -- 初始化事件函数列表
-function InitEventFuncList()
+function Main.initEventFuncList()
   -- 当前游戏模式
   local index = Runtiming.curModeIndex
   local curGameMode = Config.gameModeList[index]
@@ -358,11 +356,36 @@ function InitEventFuncList()
     end
     Runtiming.eventFuncList[k] = t
   end
-  PrintInfo()
+  Main.printInfo()
+end
+
+-- 初始化游戏模式下标
+function Main.initGameModeIndex()
+  Runtiming.curModeIndex = Config.defaultGameModeIndex or 1
+end
+
+-- 初始化按键事件下标
+function Main.initEventIndex()
+  for k, v in pairs(Config.defaultEventIndex) do
+    Runtiming.eventIndex[k] = v
+  end
+end
+
+-- 初始化试炼岛卡片下标
+function Main.initCardIndex()
+  Runtiming.curCardIndex = Config.defaultCardIndex
+end
+
+-- 初始化函数
+function Main.init()
+  Main.initGameModeIndex()
+  Main.initEventIndex()
+  Main.initCardIndex()
+  Main.initEventFuncList()
 end
 
 -- 更新按键绑定事件函数下标
-function IncreaseEventIndex(key)
+function Main.increaseEventIndex(key)
   local len = #Runtiming.eventFuncList[key]
   if len == 0 then
     return false
@@ -370,145 +393,139 @@ function IncreaseEventIndex(key)
   local nextIndex = (Runtiming.eventIndex[key] + 1) % (len + 1)
   local realIndex = nextIndex == 0 and 1 or nextIndex
   Runtiming.eventIndex[key] = realIndex
-  PrintInfo()
+  Main.printInfo()
 end
 
 -- 重置按键绑定事件函数下标
-function ResetEventIndex(key)
+function Main.resetEventIndex(key)
   local len = #Runtiming.eventFuncList[key]
   if len == 0 then
     return false
   end
   Runtiming.eventIndex[key] = 1
-  PrintInfo()
+  Main.printInfo()
 end
 
 -- 更新游戏模式
-function ChangeGameMode()
+function Main.nextGameMode()
   local len = #Config.gameModeList
   local nextIndex = (Runtiming.curModeIndex + 1) % (len + 1)
   local realIndex = nextIndex == 0 and 1 or nextIndex
   Runtiming.curModeIndex = realIndex
-  for k, _ in pairs(Runtiming.eventIndex) do
-    Runtiming.eventIndex[k] = 1
-  end
-  InitEventFuncList()
+  Main.initEventFuncList()
+  Main.initEventIndex()
+  Main.initCardIndex()
+end
+
+-- 重置游戏模式
+function Main.resetGameMode()
+  Main.init()
 end
 
 -- 打印当前游戏模式
-function PrintGameMode()
+function Main.printGameMode()
   local curGameMode = Config.gameModeList[Runtiming.curModeIndex]
   OutputLogMessage('\t当前游戏模式: ' .. ChineseTextMap[curGameMode] .. '\n\n')
 end
 
--- 返回固定长度的字符串
-function CompleteStr(str, num)
-  local start = string.len(str)
-  for i = start + 1, num do
-    str = str .. ' '
-  end
-  return str
-end
-
 -- 打印事件绑定信息
-function PrintEventInfo()
+function Main.printEventInfo()
   local curGameMode = Config.gameModeList[Runtiming.curModeIndex]
-  local eventList = GetTableSort(Config[curGameMode])
-  OutputLogMessage('\t按键\t\t%s\t\t中文\n', CompleteStr('方法名', 20))
-  OutputLogMessage('\t------\t\t%s\t\t------\n', CompleteStr('------', 20))
+  local eventList = Utils.getTableSort(Config[curGameMode])
+  -- 打印表头
+  OutputLogMessage('\t按键\t\t%s\t\t中文\n', Utils.completeStr('方法名', 20))
+  OutputLogMessage('\t------\t\t%s\t\t------\n', Utils.completeStr('------', 20))
+  -- 遍历事件列表并打印信息
   for _, item in ipairs(eventList) do
     local key = item.key
     local value = item.value
     local index = Runtiming.eventIndex[key]
     for i = 1, #value do
+      local eventName = Utils.completeStr(value[i], 20)
       local eventDesc = ChineseTextMap[value[i]]
-      local info = string.format('\t\t%s\t\t%s', CompleteStr(value[i], 20), eventDesc)
-      if index == i then
-        info = string.format('\tG%s =>', CompleteStr(key, 2)) .. info
-      else 
-        info = string.format('\t%s', CompleteStr('', 5)) .. info
-      end
+      local prefix = (index == i) and string.format('\tG%s =>', Utils.completeStr(key, 2)) or string.rep('\t', 2)
+      -- 打印事件信息
+      local info = string.format('%s\t%s\t\t%s', prefix, eventName, eventDesc)
       if curGameMode == 'pve' and value[index] == 'dropCard' then
         info = info .. string.format('(当前卡片下标:%d)', Runtiming.curCardIndex)
       end
-      OutputLogMessage(info .. '\n')
+      OutputLogMessage('%s\n', info)
     end
   end
 end
 
 -- 打印信息
-function PrintInfo()
-  ClearLog()
-  OutputLogMessage(' --------------------------------------------------------------------------------------------------- ')
-  OutputLogMessage('\n')
-  OutputLogMessage('\n')
-  PrintGameMode()
-  PrintEventInfo()
-  OutputLogMessage('\n')
-  OutputLogMessage(' --------------------------------------------------------------------------------------------------- ')
+function Main.printInfo()
+  if Config.openDebugger then
+    ClearLog()
+    OutputLogMessage(
+      ' --------------------------------------------------------------------------------------------------- \n\n')
+    Main.printGameMode()
+    Main.printEventInfo()
+    OutputLogMessage(
+      ' \n--------------------------------------------------------------------------------------------------- ')
+  end
+end
+
+-- 执行事件
+function Main.playEvent(key)
+  local eventIndex = Runtiming.eventIndex[key]
+  local fn = Runtiming.eventFuncList[key][eventIndex]
+  if type(fn) == 'function' then
+    fn(tonumber(key))
+  end
 end
 
 -- 运行指令
-function RunCmd(cmd)
-  local cmdGroup = string.split(cmd, '_')
-  local _type = cmdGroup[1]
-  local key = cmdGroup[2]
-  if _type == 'next' then
-    IncreaseEventIndex(key)
-  elseif _type == 'reset' then
-    ResetEventIndex(key)
-  elseif _type == 'play' then
-    local eventIndex = Runtiming.eventIndex[key]
-    local fn = Runtiming.eventFuncList[key][eventIndex]
-    if type(fn) == 'function' then
-      fn(tonumber(key))
-    end
-  elseif _type == 'changeGameMode' then
-    ChangeGameMode()
+function Main.runCmd(cmd)
+  local type, key = Utils.split(cmd, '_')
+  if type == 'next' then
+    Main.increaseEventIndex(key)
+  elseif type == 'reset' then
+    Main.resetEventIndex(key)
+  elseif type == 'play' then
+    Main.playEvent(key)
+  elseif type == 'nextGameMode' then
+    Main.nextGameMode()
+  elseif type == 'resetGameMode' then
+    Main.resetGameMode()
   end
 end
 
 -- 处理修饰符
-function ModifierHandler(modifier)
-  local cmd = Config.gBind[modifier]
+function Main.modifierHandler(modifier)
+  local cmd = Config.modifierMap[modifier]
   if cmd then
-    RunCmd(cmd)
+    Main.runCmd(cmd)
   end
 end
 
---------------------   end   --------------------
-
+-- [[  罗技脚本模块  ]]
 -- 打开对鼠标左键的监听
 EnablePrimaryMouseButtonEvents(true)
 
 -- 设置随机数种子
 math.randomseed(GetDate("%H%M%S"):reverse())
 
--- 初始化事件函数列表
-InitEventFuncList()
+Main.init()
 
 -- 监听事件
 function OnEvent(event, arg, family)
-  if arg == 2 then
-    arg = 3
-  elseif arg == 3 then
-    arg = 2
-  end
-
+  arg = arg == 2 and 3 or 2
   if event == 'MOUSE_BUTTON_PRESSED' and arg >= 1 and arg <= 11 and family == 'mouse' then
     -- 监听3-11的可绑定按键
     local modifier = 'G' .. arg
     for i = 1, #ModifierList do
-      if (IsPressed(ModifierList[i])) then
+      if (Utils.isKeyPressed(ModifierList[i])) then
         -- 其中某一个修饰符被按下
         modifier = ModifierList[i] .. '+' .. modifier
         break
       end
     end
     local isChange = string.find(modifier, '+')
-    if isChange or IsOpenMacro() then
+    if isChange or Utils.isOpenMacro() then
       -- 事件属于切换事件或已开启宏按钮
-      ModifierHandler(modifier)
+      Main.modifierHandler(modifier)
     end
   end
 end
